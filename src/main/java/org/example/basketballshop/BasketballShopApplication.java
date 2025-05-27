b1086979e72421f01dc0f0ca3659642ad0b90caf
Damir Rakhmatullin <damirrakhmatuliin@yandex.ru>
2025-05-27 12:35:07 +0300

    initial commit

---
+++ src/main/java/org/example/basketballshop/BasketballShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.basketballshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketballShopApplication.class, args);
+    }
+
+}
